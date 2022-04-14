6944be10787aff1d1c1410466c0b2f53b965a1ee
Nicolas Maignan <67645518+Nifra6@users.noreply.github.com>
2022-04-14 17:25:42 +0200

    Suppression de fichier inutile

--- src/pixel_par_pixel/SfS_multivues_homographie_pixel.m
+++ src/pixel_par_pixel/SfS_multivues_homographie_pixel.m
@@ -7,27 +7,24 @@ H = taille_ecran(4);
 
 %% Données
 load ../../data/donnees_calotte;
+% Les images
 I_1 = I(:,:,1);
 I_2 = I(:,:,2);
+% Les masques des images
 masque_1 = masque(:,:,1);
 masque_2 = masque(:,:,2);
-R_2 = R(:,:,1);
+% La pose
+R_2 = inv(R(:,:,1));
 t_2 = t(:,1);
-[dx_I_2, dy_I_2] = gradient(I_2);
+% Le gradient de l'image 2
+dx_I_2 = dx_I(:,:,2);
+dy_I_2 = dy_I(:,:,2);
+%[dy_I_2, dx_I_2] = gradient(I_2);
 
 %% Paramètres
-valeurs_z = 60:1:120;	% Les valeurs de profondeurs utilisées
-lambda = 0;				% Paramètre pour l'évaluation du score de la reprojection
-deuxieme_image = 0;		% Si oui, utilisation d'une deuxième rotation
-range = 1;				% Voisinage à prendre en compte
-
-%% Variables utiles
-[i_1_liste,j_1_liste] = find(masque_1);
-nb_pixels_utilises = size(i_1_liste,1);
-nb_profondeurs_testees = size(valeurs_z,2);
-scores = 10 * ones(nb_pixels_utilises, nb_profondeurs_testees);
-liste_p_estimes = zeros(nb_pixels_utilises, nb_profondeurs_testees);
-liste_q_estimes = zeros(nb_pixels_utilises, nb_profondeurs_testees);
+valeurs_z 		= 60:1:120;	% Les valeurs de profondeurs utilisées
+range			= 4;		% Voisinage à prendre en compte
+affichage_log	= 1;
 
 %% Algorithme
 while (1)
@@ -38,141 +35,130 @@ while (1)
 	pos = P.Position;
 	i_1 = round(pos(2));
 	j_1 = round(pos(1));
-	grad_I_1 = [dy_I_1(i_1,j_1); dx_I_1(i_1,j_1)];
+	grad_I_1 = [dx_I_1(i_1,j_1); dy_I_1(i_1,j_1)];
 
-	% Sélection d'une profondeur
-	for indice_z_tilde = 1:nb_profondeurs_testees
-		z_tilde = valeurs_z(indice_z_tilde);
-		% Changements de repère
-		P_1_tilde = [j_1 - u_0; i_1 - v_0; z_tilde];
-		P_2_tilde = R_2' * P_1_tilde;
-		i_2_tilde = round(P_2_tilde(2) + v_0);
-		j_2_tilde = round(P_2_tilde(1) + u_0);
+	% Récupération de la profondeur
+	z = Z_1(i_1,j_1);
 
-		% Évalution de la position du point trouvé dans les dimensions des images
-		condition_image_2 = i_2_tilde > 0 & i_2_tilde <= size(masque_2,1) & j_2_tilde > 0 & j_2_tilde <= size(masque_2,2);
+	% Changements de repère
+	P_1 = [i_1 - u_0; j_1 - v_0; z];
+	P_2 = R_2 * P_1;
+	i_2 = round(P_2(1) + u_0);
+	j_2 = round(P_2(2) + v_0);
 
-		% Si le point reprojeté tombe sur le masque de la deuxième image
-		if condition_image_2 & masque_2(i_2_tilde,j_2_tilde)
+	% Vérification si pixel hors image
+	condition_image = i_2 > 0 & i_2 <= size(masque_2,1) & j_2 > 0 & j_2 <= size(masque_2,2);
 
-			grad_I_2_tilde = [dx_I_2(i_2_tilde,j_2_tilde); dy_I_2(i_2_tilde,j_2_tilde)];
+	% Si le point reprojeté tombe sur le masque de la deuxième image
+	if condition_image & masque_2(i_2,j_2)
 
-			coefficient_p_q = R_2(3,1) * grad_I_2_tilde(1) + R_2(3,2) * grad_I_2_tilde(2);
-			second_membre = grad_I_1 - R_2(1:2,1:2) * grad_I_2_tilde;
+		grad_I_2 = [dx_I_2(i_2,j_2); dy_I_2(i_2,j_2)];
 
-			% Si pas de division par 0, on calcule le score associé
-			if abs(coefficient_p_q) > 0
-				p_estime = second_membre(1) / coefficient_p_q;
-				q_estime = second_membre(2) / coefficient_p_q;
+		denominateur_pq = R_2(1:2,3)' * grad_I_2;
+		numerateur_pq = grad_I_1 - R_2(1:2,1:2)' * grad_I_2;
 
-				%liste_p_estimes(indice_pixel, indice_z_tilde) = p_estime;
-				%liste_q_estimes(indice_pixel, indice_z_tilde) = q_estime;
+		% Si pas de division par 0, on continue
+		if abs(denominateur_pq) > 0
 
-				% Calcul du plan
-				normale = (1 / (p_estime^2 + q_estime^2 + 1)) * [p_estime ; q_estime ; 1];
-				d_equation_plan = -P_1_tilde' * normale;
+			% Estimation de la pente
+			p_estime = -numerateur_pq(1) / denominateur_pq;
+			q_estime = -numerateur_pq(2) / denominateur_pq;
 
-				% Calcul transformation géométrique
-				v_1_decales = i_1-v_0-range:i_1-v_0+range;
-				u_1_decales = j_1-u_0-range:j_1-u_0+range;
-				[u_1_decales, v_1_decales] = meshgrid(u_1_decales,v_1_decales); %ok
-				z_1_decales = -(d_equation_plan + normale(1) * u_1_decales(:) + normale(2) * v_1_decales(:)) / normale(3);
-				z_tilde;
+			% Calcul du plan au pixel considéré
+			normale = (1 / (p_estime^2 + q_estime^2 + 1)) * [p_estime ; q_estime ; 1];
+			if (affichage_log)
+				disp("===== Comparaison des normales")
+				reshape(N_1(i_1,j_1,:),3,1) - normale 
+			end
+			d_equation_plan = -P_1' * normale;
 
-				% Reprojection du voisinage
-				P_1_voisinage = [u_1_decales(:)' ; v_1_decales(:)' ; z_1_decales'];
-				P_2_voisinage = R_2' * P_1_voisinage;
-				i_2_voisinage = round(P_2_voisinage(2,:) + v_0);
-				j_2_voisinage = round(P_2_voisinage(1,:) + u_0);
-				i_2_tilde;
-				j_2_tilde;
+			% Calcul de la transformation géométrique
+			i_1_decales = i_1-u_0-range:i_1-u_0+range;
+			j_1_decales = j_1-v_0-range:j_1-v_0+range;
+			[i_1_decales, j_1_decales] = meshgrid(i_1_decales,j_1_decales);
+			z_1_decales = -(d_equation_plan + normale(1) * i_1_decales(:) + normale(2) * j_1_decales(:)) / normale(3);
+			if (affichage_log)
+				disp("===== Profondeurs z")
+				z
+				reshape(z_1_decales, 2*range+1, 2*range+1)
+			end
 
+			% Reprojection du voisinage
+			P_1_voisinage = [i_1_decales(:)' ; j_1_decales(:)' ; z_1_decales'];
+			P_2_voisinage = R_2 * P_1_voisinage;
+			i_2_voisinage = round(P_2_voisinage(1,:) + u_0);
+			j_2_voisinage = round(P_2_voisinage(2,:) + v_0);
+			if (affichage_log)
+				disp("===== Les i du voisinage")
+				i_2
+				i_2_voisinage_re = reshape(i_2_voisinage, 2*range+1, 2*range+1)
+				disp("===== Les j du voisinage")
+				j_2
+				j_2_voisinage_re = reshape(j_2_voisinage, 2*range+1, 2*range+1)
+				disp("===== Le contour du voisinage")
+				i_2_voisinage_re
+				[i_2_limites, j_2_limites] = limites_voisinage(i_2_voisinage_re+u_0,j_2_voisinage_re+v_0);
+				i_2_limites
+			end
 
-
-				%if (indice_pixel == 12475 && z_tilde == round(Z_1(i_1,j_1)))
-				if (z_tilde == round(Z_1(i_1,j_1)))
-					z_1_decales
-					z_tilde
-					reshape(i_2_voisinage, 2*range + 1, 2*range + 1) - i_2_tilde
-					reshape(j_2_voisinage, 2*range + 1, 2*range + 1) - j_2_tilde
-					figure;
-					subplot(2,2,1);
-					imshow(I_1(i_1-range:i_1+range,j_1-range:j_1+range));
-					subplot(2,2,2);
-					i_bruh = reshape(interp2(I_2, i_2_voisinage(:), j_2_voisinage(:),'nearest'),2*range+1,2*range+1);
-					i_bruh
-					imshow(i_bruh);
-					subplot(2,2,3);
-					imshow(I_1);
-					axis on
-					hold on;
-					i_1_limite = [i_1 - range ; i_1 - range ; i_1 + range ; i_1 + range];
-					j_1_limite = [j_1 - range ; j_1 + range ; j_1 + range ; j_1 - range];
-					fill(i_1_limite,j_1_limite,'g')
-					plot(i_1,j_1, 'r+', 'MarkerSize', 30, 'LineWidth', 2)
-					hold off;
-					subplot(2,2,4);
-					imshow(I_2)
-					axis on
-					hold on;
-					i_2_voisinage = reshape(i_2_voisinage, 2*range+1, 2*range+1);
-					j_2_voisinage = reshape(j_2_voisinage, 2*range+1, 2*range+1);
-					i_2_limite = [i_2_voisinage(1,1) ; i_2_voisinage(1,end) ; i_2_voisinage(end,end) ; i_2_voisinage(end,1)];
-					j_2_limite = [j_2_voisinage(1,1) ; j_2_voisinage(1,end) ; j_2_voisinage(end,end) ; j_2_voisinage(end,1)];
-					fill(i_2_limite,j_2_limite,'g')
-					plot(i_2_tilde,j_2_tilde, 'r+', 'MarkerSize', 30, 'LineWidth', 2)
-					hold off;
-				end
-				%H_ij = homographie * [i_1 ; j_1 ; 1];
-				%G_ij = H_ij(1:2) / H_ij(3);
+			% Récupération des niveaux de gris dans l'image 2 du voisinage	
+			I_2_voisinage = reshape(interp2(I_2, j_2_voisinage(:), i_2_voisinage(:),'cubic'),2*range+1,2*range+1)';
+			if (affichage_log)
+				disp("===== Différences entre les images")
+				diff = I_1(i_1-range:i_1+range,j_1-range:j_1+range) - I_2_voisinage
+				sum(diff,"all")
+			end
 
 
-				residu_1 = I_1(i_1,j_1) - 1 / sqrt(p_estime^2 + q_estime^2 + 1);
-				%residu_1 = I_1(i_1,j_1) - interp2(I_2,G_ij(2),G_ij(1),'nearest');
-				%residu_2 = I_1(i_1,j_1) - I_2(i_2_tilde,j_2_tilde);
-				residu_2 = 1;
+			% TODO mettre au propre
+			if (z == Z_1(i_1,j_1))
+				% Affichage des résultats
+				figure;
+	
+				% Image 1
+				subplot(2,2,1);
+				imshow(I_1(i_1-range:i_1+range,j_1-range:j_1+range));
+				title("Image 1 au voisinage")
 
-				%scores(indice_pixel, indice_z_tilde) = residu_1^2 + lambda * residu_2^2;
+				% Image 2
+				subplot(2,2,2);
+				imshow(I_2_voisinage);
+				title("Image 2 au voisinage")
+
+				% Voisinage 1
+				subplot(2,2,3);
+				imshow(I_1);
+				axis on
+				hold on;
+				i_1_voisinage = i_1-range:i_1+range;
+				j_1_voisinage = j_1-range:j_1+range;
+				[i_1_voisinage, j_1_voisinage] = meshgrid(i_1_voisinage,j_1_voisinage);
+				[i_1_limites, j_1_limites] = limites_voisinage(i_1_voisinage,j_1_voisinage);
+				fill(j_1_limites,i_1_limites,'g');
+				plot(j_1,i_1, 'r+', 'MarkerSize', 30, 'LineWidth', 2);
+				title("Localisation du voisinage sur l'image 1");
+				hold off;
+
+				% Voisinage 2
+				subplot(2,2,4);
+				imshow(I_2)
+				axis on
+				hold on;
+				[i_2_limites, j_2_limites] = limites_voisinage(i_2_voisinage_re,j_2_voisinage_re);
+				fill(j_2_limites,i_2_limites,'g');
+				plot(j_2,i_2, 'r+', 'MarkerSize', 30, 'LineWidth', 2);
+				title("Localisation du voisinage sur l'image 2");
+				hold off;
 			end
 		end
-
 	end
+
 	disp("Appuyez sur une touche pour recommencer.")
 	pause;
 	close all;
 end
 
-
-%% Résultats
-% Sélection des profondeurs avec le score minimal
-[A, indices_min] = min(scores, [], 2);
-z_in = transpose(valeurs_z(indices_min));
-z = zeros(256, 256);
-z(find(masque_1)) = z_in;
-
-% Affichage
-%figure('Name','Relief','Position',[0,0,0.33*L,0.5*H]);
-%plot3(X,Y,z,'k.');
-%%plot3(X,Y,Z_1,'k.');
-%xlabel('$x$','Interpreter','Latex','FontSize',30);
-%ylabel('$y$','Interpreter','Latex','FontSize',30);
-%zlabel('$z$','Interpreter','Latex','FontSize',30);
-%axis equal;
-%rotate3d;
-
-
-% Estimation de z avec p et q
-p_estimes = liste_p_estimes(indices_min);
-q_estimes = liste_p_estimes(indices_min);
-p = zeros(taille,taille);
-q = zeros(taille,taille);
-p(find(masque_1)) = p_estimes;
-q(find(masque_1)) = q_estimes;
-z_estim = integration_SCS(p,q);
-
-
-
-%% Fonction annexe
+%% Fonctions annexes
 
 function images_decalees = decaler(image)
 	[nombre_lignes, nombre_colonnes, nombres_images] = size(image);
@@ -180,4 +166,15 @@ function images_decalees = decaler(image)
 	for k = 1:nombres_images
 		images_decalees(:,:,k,:) = decalage(image(:,:,k));
 	end
+end
+
+function [i_limite, j_limite] = limites_voisinage(i_voisinage, j_voisinage)
+	i_limite = i_voisinage(1,:)';
+	i_limite = [i_limite ; i_voisinage(2:end-1,end)];
+	i_limite = [i_limite ; wrev(i_voisinage(end,:))'];
+	i_limite = [i_limite ; wrev(i_voisinage(2:end-1,1))];
+	j_limite = j_voisinage(1,:)';
+	j_limite = [j_limite ; j_voisinage(2:end-1,end)];
+	j_limite = [j_limite ; wrev(j_voisinage(end,:))'];
+	j_limite = [j_limite ; wrev(j_voisinage(2:end-1,1))];
 end
